171c3d110c29917adbab6de5a709bd19a68895a2
qqqwwwddd <qqqwwwddd1@gmail.com>
2021-09-13 18:04:46 +0900

    movieproject 수정

--- movie_api.py
+++ movie_api.py
@@ -1,3 +1,4 @@
+from movie_model import MovieModel
 import requests
 
 # pip instrall requests
@@ -10,4 +11,15 @@ def callMovieApi(page=1):  # movie를 받기위한 함수
 
     responseDict = response.json() # 딕셔너리 타입으로 변환
     movies = responseDict["data"]["movies"] # list 타입
-    return movies
+    return convert_model(movies)
+
+
+def convert_model(movies): # model로 변경하는 함수
+    list = []
+
+    for movie in movies:  # movie를 movie_model에 옮겨 담아야함 
+        movie_model = MovieModel(movie["title"], movie["rating"], movie["medium_cover_image"])
+        list.append(movie_model) # list에 넣어줌
+
+    print(list)
+    return list
---
+++ movie_model.py
@@ -0,0 +1,7 @@
+
+class MovieModel:
+    def __init__(self, title, rating, medium_cover_image):
+        self.title = title
+        self.rating = rating
+        self.medium_cover_image = medium_cover_image
+        
